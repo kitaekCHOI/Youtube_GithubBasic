1ae4c069dbf5f8ce83f40a0eaaf8f1bfa6d66805
kitaekCHOI <ktchoi0625@empal.com>
2025-02-28 11:02:20 +0900

    Modify New Print

--- Upload_test.py
+++ Upload_test.py
@@ -1,5 +1,8 @@
-print('Github 업로드용 테스트 파일!')
+print('Github 업로드용 테스트 파일!')  
 
-print('Github 업로드용 테스트 xxxxx파일!')
+print('Github 업로드용 테스트 xxxxx파일!')  
 
 print('Github 업로드용 테스트 yyyyy파일!')
+
+print('Github 업로드용 테스트 zzzzz파일!')
+
